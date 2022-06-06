a63218493d5192021c19c272d37c4914ff890f1d
Alexandre Visnardi <avisnardi@gmail.com>
2022-06-05 23:14:28 -0300

    Primeiro commit do projeto Java.

---
+++ api/src/main/java/br/com/wsn/mylist/api/data/model/MyList.java
@@ -0,0 +1,36 @@
+package br.com.wsn.mylist.api.data.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.Id;
+
+@Entity
+@Table(name = "list")
+public class MyList {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer Id;
+	
+	private String Name;
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	 
+}
